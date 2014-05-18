03edc48666f5ceda95083995f8a0c63469acc8f0
kb9zzw <kb9zzw@gmail.com>
2014-05-17 20:08:00 -0400

    mac tweaks for git completion

--- shell/bash/mac.sh
+++ shell/bash/mac.sh
@@ -1,8 +1,12 @@
 # os x bash_completion
 if which brew > /dev/null 2>&1
 then
-  if [ -f $(brew --prefix)/etc/bash_completion ]; then
-      . $(brew --prefix)/etc/bash_completion
+  if [ -f $(brew --prefix)/etc/bash_completion.d/git-completion.bash ]; then
+      . $(brew --prefix)/etc/bash_completion.d/git-completion.bash
+  fi
+  if [ -f $(brew --prefix)/etc/bash_completion.d/git-prompt.sh ] # mac osx brew
+  then
+    . $(brew --prefix)/etc/bash_completion.d/git-prompt.sh ] 
   fi
   export PATH="/usr/local/bin":$PATH
 fi
