9dfb0eed081ce1cddc869819d5bae3ffd06dcb64
Jon Burroughs <kb9zzw@gmail.com>
2019-07-02 21:26:40 -0500

    added option to disable vscode

--- vscode/vscode_extensions.sh
+++ vscode/vscode_extensions.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -n "${VSCODE_DISABLED}" ]; then
+  echo "vscode extension management disabled"
+  exit 1
+fi
+
 for ext in $(cat ~/.vscode/extensions.txt); do
   code --install-extension $ext
 done
